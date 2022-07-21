427019a4fa418f5d0872c96ee01f872402e2bbcc
AdrieneTZ <tznali17@gmail.com>
2022-07-21 16:40:00 +0800

    feat: modify api unauthorized response

--- middleware/api-auth.js
+++ middleware/api-auth.js
@@ -1,6 +1,16 @@
 const passport = require('../config/passport')
 
-const authenticated = passport.authenticate('jwt', { session: false })
+// const authenticated = passport.authenticate('jwt', { session: false })
+const authenticated = (req, res, next) => {
+  const callbackFn = (error, user) => {
+    if (error || !user) return res.status(401).json({ status: 'error', message: 'unauthorized' })
+
+    next()
+  }
+  // passport.authenticate() is a callback middleware, so at the and adding (req, res, next)
+  passport.authenticate('jwt', { session: false }, callbackFn)(req, res, next)
+}
+
 const authenticatedAdmin = (req, res, next) => {
   if (req.user && req.user.isAdmin) return next()
 
